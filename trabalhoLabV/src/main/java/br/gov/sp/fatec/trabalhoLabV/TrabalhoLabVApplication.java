696d3f0b934fa45c8756731afe2fc55c150b0f92
Paulo Henrique Filipini <45483678+PauloFilipini@users.noreply.github.com>
2021-08-27 18:13:57 -0300

    Add files via upload

---
+++ trabalhoLabV/src/main/java/br/gov/sp/fatec/trabalhoLabV/TrabalhoLabVApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.trabalhoLabV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoLabVApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoLabVApplication.class, args);
+	}
+
+}
---
+++ trabalhoLabV/src/test/java/br/gov/sp/fatec/trabalhoLabV/TrabalhoLabVApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.trabalhoLabV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoLabVApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
